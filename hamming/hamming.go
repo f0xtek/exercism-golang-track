51cdfdfa47ea19ad55bba830adf940cfeb36729d
Luke Anderson <landerson61@gmail.com>
2018-11-06 22:41:34 +0000

    Refactor: hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,17 +1,15 @@
 package hamming
 
-import (
-	"fmt"
-)
+import "errors"
 
 // Distance returns the Hamming distance for 2 given strings (DNA strands)
 // or and error if the length of a and b are not equal
 func Distance(a, b string) (int, error) {
 	hammingDistance := 0
 	if len(a) != len(b) {
-		return 0, fmt.Errorf("len(a) != len(b)")
+		return 0, errors.New("len(a) != len(b)")
 	}
-	for i := 0; i < len(a); i++ {
+	for i := range a {
 		if a[i] != b[i] {
 			hammingDistance++
 		}
